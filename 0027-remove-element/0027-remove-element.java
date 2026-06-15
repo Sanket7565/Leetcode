b07932bb765f0b014c7909ce7de28d295c2ccd15
Sanket Padavale. <121641782+Sanket7565@users.noreply.github.com>
2026-06-15 15:16:52 +0530

    Time: 0 ms (100%), Space: 43.6 MB (41.81%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int removeElement(int[] nums, int val) 
+    {
+        //Arrays.sort(nums);
+
+        int left=0;
+       int right=0;
+        //int count=0;
+
+        while(right<=nums.length-1)
+        {
+            if(nums[right]!=val)
+            {
+                int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+
+                left++;
+                right++;
+            }
+            else
+            {
+                right++;
+            }
+        
+        }
+
+        return left;
+        
+    }
+}
