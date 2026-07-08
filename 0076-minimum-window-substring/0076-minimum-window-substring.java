068defe7234d44c12af3b5aaf23b87abd5c5113d
Sanket Padavale. <121641782+Sanket7565@users.noreply.github.com>
2026-07-08 11:03:19 +0530

    Time: 66 ms (15.74%), Space: 46.4 MB (60.35%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,59 @@
+class Solution {
+    public String minWindow(String s, String t) 
+    {
+        int needed[]=new int[256];
+        int have[]=new int [256];
+        int low=0;
+        int minLength=Integer.MAX_VALUE;
+        int startIndex=0;
+
+
+        for (int i=0; i<t.length();i++)
+        {
+            char ch= t.charAt(i);
+            needed[ch]++;
+        }
+
+
+        for(int high=0;high<s.length();high++)
+        {
+            char ch=s.charAt(high);
+            have[ch]++;
+
+            while(isValid(have,needed))
+            {
+
+                //check if current length (high-low+1) iss less than previousaly calculated length(minlength)
+                if(minLength>(high-low+1)) //if yes
+                {
+                    minLength=high-low+1;  // store the leess length in minlnegth
+                    startIndex=low;        //store the starting point as low, window always starts from low
+                }
+
+                have[s.charAt(low)]--;
+                low++;      
+
+            }
+
+        }
+
+        return minLength==Integer.MAX_VALUE? "" : s.substring(startIndex,startIndex+minLength); //start index =3 minlength=
+
+        
+    }
+
+    boolean isValid(int [] have, int []needed)  
+
+        {
+            for(int i=0;i<256;i++)
+            {
+                if(have[i]<needed[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }   
+}
