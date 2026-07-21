6b8668764735252bfa3fc6ea0293c94f8e180326
Sanket Padavale. <121641782+Sanket7565@users.noreply.github.com>
2026-07-21 15:49:32 +0530

    Time: 2 ms (70.83%), Space: 47.2 MB (53.52%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,24 +1,26 @@
 class Solution {
-    public int maxProduct(int[] nums)
+    public int maxProduct(int[] nums) 
     {
-        int currentMaxAns= nums[0];
-        int currentMinAns= nums[0];
+        int currentMax=nums[0];
+        int currentMin=nums[0];
         int bestAns=nums[0];
 
-        for (int i=1;i<nums.length; i++)
+        for(int i=1;i<nums.length;i++)
         {
-            int contributeToMax= currentMaxAns*nums[i];
-            int contributeToMin= currentMinAns*nums[i];
-            int self= nums[i];
+            int contributeMax=currentMax*nums[i];
 
-            currentMaxAns= Math.max(self,Math.max(contributeToMax,contributeToMin));
-            currentMinAns= Math.min(self,Math.min(contributeToMax,contributeToMin));
+            int contributeMin=currentMin*nums[i];
 
-            bestAns= Math.max(bestAns,currentMaxAns); 
+            int self=nums[i];
+
+            currentMin=Math.min(self,Math.min(contributeMax,contributeMin));
+
+            currentMax=Math.max(self,Math.max(contributeMax,contributeMin));
+
+            bestAns=Math.max(currentMax,bestAns);
         }
 
         return bestAns;
         
     }
-        
-    }
+}
